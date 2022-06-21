1db49d95f6627f8542ef4ae0a61a2f7674e5860f
minshadk <minshadk13@gmail.com>
2022-06-21 19:28:28 +0530

    comments added

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import { WebSocketServer } from "ws";
 const socket = new WebSocketServer({ port: 8080 });
 
 socket.on("connection", (client) => {
-  console.log("something hit the server");
   client.on("message", (message, isBinary) => {
     [...socket.clients]
       .filter((c) => c !== client)
